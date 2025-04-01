593e59e8cbe6d9ec60e38ce2cabc4bfb5a829f89
CosmicSand <skyjlen@gmail.com>
2025-04-01 21:32:06 +0300

    major

--- src/ts/info.ts
+++ src/ts/info.ts
@@ -2,4 +2,13 @@ export function swipingByNextButton(event: Event) {
   const nextBtn = document.querySelector("[data-next-button]");
   console.log(event.target !== nextBtn);
   if (event.target !== nextBtn) return;
+
+  const greetingsFirstPart = document.querySelector("[data-first-part]");
+  const greetingsSecondPart = document.querySelector("[data-second-part]");
+  greetingsFirstPart?.classList.add("anime");
+  greetingsFirstPart?.addEventListener("animationend", () => {
+    greetingsFirstPart?.classList.add("hidden");
+    greetingsFirstPart?.classList.remove("anime");
+    greetingsSecondPart?.classList.remove("hidden");
+  });
 }
