2e0f884a975b27fc9fe68b24712ad5450f525bb1
CosmicSand <skyjlen@gmail.com>
2025-07-31 21:37:55 +0300

    Change login button type and remove unused code
    Updated the login button in index.html from type="submit" to type="button" to prevent form submission by default. Removed commented-out sorting function from main.ts for code cleanliness.

--- src/main.ts
+++ src/main.ts
@@ -9,25 +9,3 @@ submitForLoginEventListener();
 clickEventListenerGeneral();
 submitModalEventListener();
 closeByClickModalEventListener();
-
-// ==== Функція сортування
-// function sorting() {
-//   const allSensors: NodeListOf<Element> = document.querySelectorAll(
-//     "[data-sensor='true']"
-//   );
-//   const arrayOfAllSensors = [...allSensors].toSorted((a, b) =>
-//     a.id.localeCompare(b.id)
-//   );
-//   const ownersControlAreaForSensors = document.querySelector(
-//     `[data-sensor=${import.meta.env.VITE_USER}]`
-//   ) as HTMLDivElement;
-
-//   // Clear the container before appending new elements
-//   ownersControlAreaForSensors.innerHTML = "";
-
-//   // Append each sensor element to the container
-//   arrayOfAllSensors.forEach((el) => {
-//     const element = el as HTMLDivElement;
-//     ownersControlAreaForSensors.appendChild(element);
-//   });
-// }
